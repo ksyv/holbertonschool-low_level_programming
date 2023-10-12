8df48c07aedae9c65ff927be6d390dd4dc04b197
ksyv <viais.kevin@hotmail.fr>
2023-10-12 16:12:38 +0200

    change 9 to task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,12 @@
 
 #include <stdio.h>
 
-int main(void) /* entry program declaration with no variable */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
