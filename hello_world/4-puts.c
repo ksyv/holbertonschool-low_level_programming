c927dab403c6e7cde23c0c6fd0cd7aafec98e212
ksyv <viais.kevin@hotmail.fr>
2023-10-12 15:45:03 +0200

    change 5 for task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,9 +2,9 @@
 
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-	puts( "\"Programming is like building a multilingual puzzle'");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
