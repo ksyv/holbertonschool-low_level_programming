d5489af8d31058917533825bd8df36656815c5f9
ksyv <viais.kevin@hotmail.fr>
2023-10-12 16:17:09 +0200

    prog for task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/* program who print the text of task 5 */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
