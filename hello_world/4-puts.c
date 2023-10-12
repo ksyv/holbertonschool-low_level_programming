5f152a068ee22055e8f70074093350a5ccb80f73
ksyv <viais.kevin@hotmail.fr>
2023-10-12 15:22:18 +0200

    add .c for task 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/* program who print the text of task 4 */
+
+#include <stdio.h>
+
+int main();
+int puts( const char * string );
+{
+	puts( '"Programming is like building a multilingual puzzle' );
+
+	return 0;
+}
