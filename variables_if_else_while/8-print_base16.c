a6c3da32a7f41e9069b6ade7a598420eed6f4e67
ksyv <viais.kevin@hotmail.fr>
2023-10-16 14:30:46 +0200

    add for task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+	char r = '\n';
+
+	while (n < 16)
+	{
+		if (n < 10)
+		{
+		putchar(n);
+		}
+		else
+		{
+		putchar(n + 87);
+		}
+		n++;
+	}
+	putchar(r);
+	return (0);
+}
+
