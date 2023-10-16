ebaca765c67e3142c1b203ff84fb329f2e9f2a93
ksyv <viais.kevin@hotmail.fr>
2023-10-16 15:04:02 +0200

    add for task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+	char a = ' ';
+	char r = '\n';
+
+	while (n < 10)
+	{
+		putchar(n + 48);
+		if (n < 9)
+		{
+		putchar(44);
+		putchar(a);
+		}
+		n++;
+	}
+	putchar(r);
+	return (0);
+}
