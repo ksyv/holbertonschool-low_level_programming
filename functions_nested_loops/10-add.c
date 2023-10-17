a6b5e70c6b4454ba4e1515daa8a05175ed51e86b
ksyv <viais.kevin@hotmail.fr>
2023-10-17 12:27:44 +0200

    add for task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @a: first integrer
+ * @b: sec integrer
+ * Return: the result
+ */
+int add(int a, int b)
+{
+	int res = a + b;
+
+	return (res);
+}
