93465523ba8b65db6acdb025a592eb24f41cf268
ksyv <viais.kevin@hotmail.fr>
2023-10-17 09:41:59 +0200

    add for task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of a number
+ * @n: the number intergrate
+ * Return: Value of last digit
+ */
+int print_last_digit(int n)
+{
+	int l = n % 10;
+
+	_putchar(l);
+	return (l);
+}
