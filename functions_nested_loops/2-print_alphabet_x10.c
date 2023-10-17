001a23c57da42a3b3707128254c1e329d8d003a7
ksyv <viais.kevin@hotmail.fr>
2023-10-17 08:07:03 +0200

    change 0 for task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_X10 - print 10 times the alphabet in lowercase,
+ * print_alphabet_x10 - print 10 times the alphabet in lowercase,
  * followed by a new line.
  */
 void print_alphabet_x10(void)
@@ -9,13 +9,14 @@ void print_alphabet_x10(void)
 	int r = '\n';
 	int t;
 
-	for (t = 0; t < 10 ; t++)
+	for (t = 0 ; t < 10 ; t++)
 	{
 		while (n < 123)
 		{
 			_putchar (n);
 			n++;
 		}
+		n = 97;
 	}
 	_putchar (r);
 }
