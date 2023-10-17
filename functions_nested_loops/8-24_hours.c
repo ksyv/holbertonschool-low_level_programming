f560ee3de917cfed2672482f0d7c1c6bea50b1df
ksyv <viais.kevin@hotmail.fr>
2023-10-17 10:43:36 +0200

    add for task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - print every minute of 24h
+ */
+void jack_bauer(void)
+{
+	int dh, uh, dm, um;
+
+	for (dh = 0; dh < 2; dh++)
+	{
+		for (uh = 0; uh < 10; uh++)
+		{
+			for (dm = 0; dm < 6; dm++)
+			{
+				for (um = 0; um < 10; um++)
+				{
+					_putchar(dh + '0');
+					_putchar(uh + '0');
+					_putchar(':');
+					_putchar(dm + '0');
+					_putchar(um + '0');
+				}
+			}
+		}
+	};
+}
