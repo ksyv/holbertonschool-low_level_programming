ff0b949a7cf48dafbae2f7d260e15fdc95f531f1
ksyv <viais.kevin@hotmail.fr>
2023-10-17 11:48:38 +0200

    change 0 for task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,16 +4,28 @@
  */
 void times_table(void)
 {
-	int a, b, c;
+	int a, b, c, d, e;
 
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 10; b++)
 		{
 			c = a * b;
-			_putchar(c);
-			_putchar(',');
-			_putchar(' ');
+			if (c < 10)
+			{
+				_putchar(c + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				d = c / 10;
+				e = c % 10;
+				_putchar(d + '0');
+				_putchar(e + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
