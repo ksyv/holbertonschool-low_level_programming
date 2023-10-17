05a0674b1b487584f094f4e63912fdc7b9edf77f
ksyv <viais.kevin@hotmail.fr>
2023-10-17 11:07:51 +0200

    change 1 for task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,7 +6,7 @@ void jack_bauer(void)
 {
 	int dh, uh, dm, um;
 
-	for (dh = 0; dh < 3; dh++)
+	for (dh = 0; dh < 2; dh++)
 	{
 		for (uh = 0; uh < 10; uh++)
 		{
@@ -23,5 +23,21 @@ void jack_bauer(void)
 				}
 			}
 		}
-	};
+	}
+	dh = 2;
+	for (uh = 0; uh < 5; uh++)
+	{
+		for (dm = 0; dm < 6; dm++)
+		{
+			for (um = 0; um < 10; um++)
+			{
+				_putchar(dh + '0');
+				_putchar(uh + '0');
+				_putchar(':');
+				_putchar(dm + '0');
+				_putchar(um + '0');
+				_putchar('\n');
+			}
+		}
+	}
 }
