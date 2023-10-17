0cc0948402c41c474cb1bbec0216c1bdc56a8659
ksyv <viais.kevin@hotmail.fr>
2023-10-17 12:59:53 +0200

    change 0 for task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,9 +8,25 @@
  */
 void print_to_98(int n)
 {
-	for (; n < 99; n++)
+	printf("%d", n);
+	if (n != 98)
 	{
-		printf("%d, ", n);
+		if (n < 98)
+		{
+			n = n + 1;
+			for (; n < 99; n++)
+			{
+				printf(", %d", n);
+			}
+		}
+		else
+		{
+			n = n - 1;
+			for (; n > 99; n--)
+			{
+				printf(", %d", n);
+			}
+		}
 	}
 	printf("\n");
 }
