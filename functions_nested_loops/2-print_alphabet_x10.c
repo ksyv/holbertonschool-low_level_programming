3ec4a4270070ba6c16275391f63c9ef97e6d4993
ksyv <viais.kevin@hotmail.fr>
2023-10-17 08:00:33 +0200

    change the fonction name in lowercase

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
  * print_alphabet_X10 - print 10 times the alphabet in lowercase,
  * followed by a new line.
  */
-void print_alphabet_X10(void)
+void print_alphabet_x10(void)
 {
 	int n = 97;
 	int r = '\n';
