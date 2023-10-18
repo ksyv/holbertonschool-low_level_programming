8ac0f949797b5b261ea3c8c7e32455c8b0ee8337
ksyv <viais.kevin@hotmail.fr>
2023-10-18 10:44:37 +0200

    add for task 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - checks for digit character.
+ * @c: the character who checked
+ * Return: 1 if c is digit, 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if ((c > 47) && (c < 58))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
