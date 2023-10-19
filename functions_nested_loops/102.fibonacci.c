b85f910decb1c5634b84dcae1da4ff9d14a2d6dd
ksyv <viais.kevin@hotmail.fr>
2023-10-19 14:58:57 +0200

    add for task 14

---
+++ functions_nested_loops/102.fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print fibonacci's 50 first numbers
+ * followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int a, c = 0;
+	int b = 1;
+
+	for (a = 0; a <= 50; a++)
+	{
+		if (a % 2 != 0)
+		{
+			c = b + c;
+			printf("%d, ", c);
+		}
+		else
+		{
+			b = b + c;
+			printf("%d, ", b);
+		}
+	}
+	printf("\n");
+	return (0);
+}
