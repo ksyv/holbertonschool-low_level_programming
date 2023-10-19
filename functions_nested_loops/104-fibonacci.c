5368f42b68c0d1bbb708e5d37a613c277c2c869f
ksyv <viais.kevin@hotmail.fr>
2023-10-19 21:55:03 +0200

    change for task 16

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -10,38 +10,42 @@ int main(void)
 	int a = 0;
 	unsigned long int c = 0;
 	unsigned long int b = 1;
+	unsigned long int c1, c2, b1, b2;
 
-	for (a = 1; a < 98; a++)
+	for (a = 1; a < 92; a++)
 	{
 		if (a % 2 != 0)
 		{
 			c = b + c;
-			if (c < 2147483647)
-			{
-				printf("%ld, ", c);
-			}
-			else
-			{
-				printf("%ld", c / 1000000000);
-				printf("%ld, ", c % 1000000000);
-			}
+			printf("%ld, ", c);
 		}
 		else
 		{
 			b = b + c;
-			if (b < 2147483647)
-			{
-				printf("%ld, ", b);
-			}
-			else
-			{
-				printf("%ld", b / 1000000000);
-				printf("%ld, ", b % 1000000000);
-			}
+			printf("%ld, ", b);
 		}
 	}
-	b = b + c;
-	printf("%ld", b / 1000000000);
-	printf("%ld\n", b % 1000000000);
+	c1 = c / 10000000000;
+	c2 = c % 10000000000;
+	b1 = b / 10000000000;
+	b2 = b % 10000000000;
+	for (; a < 98; a++)
+	{
+		if (a % 2 != 0)
+		{
+			c1 += b1;
+			c2 += b2;
+			printf("%ld%ld, ", c1, c2);
+		}
+		else
+		{
+			b1 += c1;
+			b2 += c2;
+			printf("%ld%ld, ", b1, b2);
+		}
+	}
+	b1 += c1;
+	b2 += c2;
+	printf("%ld%ld", b1, b2);
 	return (0);
 }
