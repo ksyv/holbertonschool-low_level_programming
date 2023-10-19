cbb5b51ce51cf9958c5b23e10d55fcf5dc538f09
ksyv <viais.kevin@hotmail.fr>
2023-10-19 14:20:38 +0200

    add for task 13

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -9,12 +9,14 @@ int main(void)
 {
 	int a, b, m3, m5 = 0;
 
-	for (a = 0; a < 1024; a = a + 3)
+	while (a < 1024)
 	{
+		a = a + 3;
 		m3 = m3 + a;
 	}
-	for (b = 0; b < 1024; b = b + 5)
+	while (b < 1024)
 	{
+		b = b + 5;
 		m5 = m5 + b;
 	}
 	printf("%d\n", m3 + m5);
