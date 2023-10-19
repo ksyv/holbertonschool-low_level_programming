9177c5b4e4655c847ff51396504fab83d5eb5add
ksyv <viais.kevin@hotmail.fr>
2023-10-19 13:05:25 +0200

    change 2 for task 12

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -10,9 +10,9 @@ void print_times_table(int n)
 
 	if (n > 0 && n < 15)
 	{
-		for (a = 0; a < n; a++)
+		for (a = 0; a <= n; a++)
 		{
-			for (b = 0; b < n; b++)
+			for (b = 0; b <= n; b++)
 			{
 				if (a * b / 10 > 0)
 				{
