195a41b980555a223ae0a45d41869a74543ca77b
ksyv <viais.kevin@hotmail.fr>
2023-10-19 22:17:35 +0200

    change for task 16

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-
 /**
  * main - print fibonacci's 50 first numbers
- * followed by a new line
  * Return: 0
  */
 int main(void)
 {
 	int a = 0;
-	unsigned long int c = 0;
-	unsigned long int b = 1;
-	unsigned long int c1, c2, b1, b2;
+	unsigned long int b = 1, c = 0, c1, c2, b1, b2;
 
 	for (a = 1; a < 92; a++)
 	{
@@ -25,10 +21,10 @@ int main(void)
 			printf("%ld, ", b);
 		}
 	}
-	c1 = c / 100000000000;
-	c2 = c % 100000000000;
-	b1 = b / 100000000000;
-	b2 = b % 100000000000;
+	c1 = c / 1000000000000;
+	c2 = c % 1000000000000;
+	b1 = b / 1000000000000;
+	b2 = b % 1000000000000;
 	for (; a < 98; a++)
 	{
 		if (a % 2 != 0)
