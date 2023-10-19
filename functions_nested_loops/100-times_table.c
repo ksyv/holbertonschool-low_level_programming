3ce7393625187b229f28c7f76d59fb3b71c3341d
ksyv <viais.kevin@hotmail.fr>
2023-10-19 12:56:02 +0200

    add for task 12

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: int
+ */
+void print_times_table(int n)
+{
+	int a;
+	int b;
+
+	if (n > 0 && n < 15)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < n; b++)
+			{
+				if (a * b / 10 > 0)
+				{
+					_putchar(a * b / 10 + '0');
+				}
+				_putchar(a * b % 10);
+				if (b < n - 1)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+	}
+}
+
