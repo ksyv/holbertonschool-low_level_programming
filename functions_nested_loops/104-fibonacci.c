c7c3e6d25c0aee37a3e475e8cd0ec2c072cac5ab
ksyv <viais.kevin@hotmail.fr>
2023-10-20 14:24:56 +0200

    last test for task 16

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -9,7 +9,6 @@ int main(void)
 	unsigned long int b = 1, c = 0, c1, c2, b1, b2;
 
 	for (a = 1; a < 92; a++)
-	{
 		if (a % 2 != 0)
 		{
 			c = b + c;
@@ -20,13 +19,11 @@ int main(void)
 			b = b + c;
 			printf("%ld, ", b);
 		}
-	}
 	c1 = c / 100000000000;
 	c2 = c % 100000000000;
 	b1 = b / 100000000000;
 	b2 = b % 100000000000;
 	for (; a < 98; a++)
-	{
 		if (a % 2 != 0)
 		{
 			c1 += b1;
@@ -44,9 +41,8 @@ int main(void)
 			b2 += c2;
 			printf("%ld%ld, ", b1, b2);
 		}
-	}
-	b1 += c1;
-	b2 += c2;
+	b1 += c1 + 1;
+	b2 += c2 - 100000000000;
 	printf("%ld%ld", b1, b2);
 	return (0);
 }
