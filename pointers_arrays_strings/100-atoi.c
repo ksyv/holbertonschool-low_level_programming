12b2f68674158590ed48c0b74b2f01dbf178a29d
ksyv <viais.kevin@hotmail.fr>
2023-10-23 18:12:48 +0200

    add for task 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s: string of base
+ * Return: integrer who be convert
+ */
+int _atoi(char *s)
+{
+	int t = 0, r = 0, ns = 1;
+
+	while (s[t] != 0)
+	{
+		if (s[t] == 45)
+		{
+			ns *= -1;
+		}
+		if (s[t] > 47 && s[t] < 58)
+		{
+			r += *(s + t) - 48;
+			r *= 10;
+		}
+		t++;
+	}
+	r /= 10;
+	r *= ns;
+	return (r);
+}
