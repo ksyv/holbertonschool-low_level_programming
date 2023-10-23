3c17efbc476511cc686314342e7cd6b62d0197ab
ksyv <viais.kevin@hotmail.fr>
2023-10-23 11:58:31 +0200

    add script for task 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - funciton that takes a pointer to an int
+ * as parameter and updates the value it points to do 98.
+ * @n: n variables who give.
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
