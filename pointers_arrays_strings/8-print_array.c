f12fa6736dadf29fa01c14d2e0e9d2b7a5a2f4b8
ksyv <viais.kevin@hotmail.fr>
2023-10-23 14:47:58 +0200

    ad for task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array of integers,
+ * followed by a new line.
+ * @a: integrer who be printed
+ * @n: number of elements who be printed
+ */
+void print_array(int *a, int n)
+{
+	int t = 0;
+
+	while (t < n-1)
+	{
+		printf("%d ,", *(a + t));
+		t++;
+	}
+	printf("%d", *(a + t));
+	printf("\n");
+}
