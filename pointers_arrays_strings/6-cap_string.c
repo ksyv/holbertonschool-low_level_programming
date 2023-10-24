62d76753089e89155eabafd8837dfd05664f5939
ksyv <viais.kevin@hotmail.fr>
2023-10-24 14:08:18 +0200

    add for task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string.
+ * @a: the string analysed
+ * Return: new string
+ */
+char *cap_string(char *a)
+{
+	int t = 0;
+
+	while (a[t] != '\0')
+	{
+		if (a[t - 1] == (' ') ||
+				a[t - 1] == (' ') ||
+				a[t - 1] == (';') ||
+				a[t - 1] == ('.') ||
+				a[t - 1] == ('!') ||
+				a[t - 1] == ('?') ||
+				a[t - 1] == ('"') ||
+				a[t - 1] == ('(') ||
+				a[t - 1] == (')') ||
+				a[t - 1] == ('{') ||
+				a[t - 1] == ('}') ||
+				a[t - 1] == ('	') ||
+				a[t - 1] == ('\n'))
+		{
+			if (*(a + t) > 96 && *(a + t) < 123)
+			{
+				*(a + t) -= 32;
+			}
+		}
+		t++;
+	}
+	return (a);
+}
