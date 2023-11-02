3e4f6fc3c9777e56328903b4312756487457e321
ksyv <viais.kevin@hotmail.fr>
2023-11-02 12:51:04 +0100

    change for the long string in 100

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * is_palindrome - return if testpalind is ok
  * @s: the string of character
@@ -7,34 +8,32 @@
 int is_palindrome(char *s)
 {
 	int l = _strlen_recursion(s);
-	int t = l / 2 - 1;
 	int u = 0;
 
 	if (*s == '\0')
 	{
 		return (1);
 	}
-	return (testpalind(s, l, u, t));
+	return (testpalind(s, l, u));
 }
 /**
  *  testpalind - test if is a palind
  *  @l: nmb of charac
  *  @s: string of cara
- *  @t: half l
  *  @u: new index
  *  Return: 1 if s is a palind, 0 if not
  */
-int testpalind(char *s, int l, int u, int t)
+int testpalind(char *s, int l, int u)
 {
 	if (*(s + u) != *(s + (l - 1)))
 	{
 		return (0);
 	}
-	if  (u == t || l == u)
+	if  (u >= l)
 	{
 		return (1);
 	}
-	return (testpalind(s, l - 1, u + 1, t));
+	return (testpalind(s, l - 1, u + 1));
 }
 /**
  * _strlen_recursion - returns the length of a string
@@ -43,10 +42,9 @@ int testpalind(char *s, int l, int u, int t)
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		return (0);
-	}
-	_strlen_recursion(s + 1);
-	return (_strlen_recursion(s + 1) + 1);
+        if (!(*s))
+        {
+                return (0);
+        }
+        return (_strlen_recursion(s + 1) + 1);
 }
