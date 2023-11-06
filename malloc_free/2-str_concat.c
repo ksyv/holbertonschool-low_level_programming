d3d741860fcb27c5b89935855065a257e3215d4b
ksyv <viais.kevin@hotmail.fr>
2023-11-06 13:50:18 +0100

    change 3 for task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,6 +12,10 @@ char *str_concat(char *s1, char *s2)
 	char *st;
 	int length, l1, l2, i;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 	l1 = i;
 	for (i = 0; s2[i] != '\0'; i++)
