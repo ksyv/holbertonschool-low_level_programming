1558fbc5063a6f03911fe3da94c6e49f8dd7dabf
ksyv <viais.kevin@hotmail.fr>
2023-11-06 13:14:18 +0100

    change for task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,19 +10,20 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *st;
-	int length, l1, l2, i;
+	int length, l1, l2, i, j;
 
 	for (i = 0; s1[i]; i++)
 	l1 = i;
-	for (i = 0; s2[i] == '\0'; i++)
+	for (i = 0; s2[i]; i++)
 	l2 = i;
 	length = l1 + l2;
-	st = malloc(length * sizeof(char));
-	if (st == NULL)
-		return (NULL);
+	st = malloc((length + 1) * sizeof(char));
 	for (i = 0; i <= l1; i++)
 		st[i] = s1[i];
-	for (i = 0; i <= l2; i++)
-		st[i + l1 + 1] = s2[i];
+	for (j = 0; i <= length + 1; i++)
+	{
+		st[i] = s2[j];
+		j++;
+	}
 	return (st);
 }
