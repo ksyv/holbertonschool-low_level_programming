68f95ab0947704c5160f3552066067a0f0a32e0f
ksyv <viais.kevin@hotmail.fr>
2023-11-07 14:44:54 +0100

    add for task 0 and 1

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -21,6 +21,8 @@ char *str_concat(char *s1, char *s2)
 	for (len2 = 0; s2[len2]; len2++)
 		continue;
 	st = malloc((len1 + len2 + 1) * sizeof(char));
+	if (st == NULL)
+		return(NULL);
 	for (len1 = 0; s1[len1]; len1++)
 		st[len1] = s1[len1];
 	for (len2 = 0; s2[len2]; len2++)
---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * char *string_nconcat - concatenates two strings
+ * @s1 : string 1
+ * @s2 : string 2
+ * @n : bytes of string 2 would be concatenated
+ * Return: pointer to the memory
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat = NULL;
+	unsigned int index1, index2;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (index1 = 0; s1[index1]; index1++)
+		continue;
+	for (index2 = 0; s2[index2]; index2++)
+		continue;
+	if (n > index2)
+		n = index2;
+	concat = malloc(sizeof(char) * (index1 + n + 1));
+	if (concat == NULL)
+		return (NULL);
+	for (index1 = 0; s1[index1]; index1++)
+		concat[index1] = s1[index1];
+	for (index2 = 0; index2 <= n; index2++)
+		concat[index1 + index2] = s2[index2];
+	return (concat);
+}
