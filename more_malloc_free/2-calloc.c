5924778d9559908f63c8da746bb37a1b7b194dfa
ksyv <viais.kevin@hotmail.fr>
2023-11-08 10:13:38 +0100

    add files for 2-calloc.c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *_calloc - allocates memory for an array using malloc
+ * @nmemb: number of array's element
+ * @size: size bytes of the elements
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *memcalloc = NULL;
+	char *set = NULL;
+	unsigned int index = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	memcalloc = malloc(size * nmemb);
+	if (memcalloc == NULL)
+		return (NULL);
+	set = memcalloc;
+	for (index = 0; index < (size * nmemb); index++)
+		set[index] = '\0';
+	return (memcalloc);
+}
