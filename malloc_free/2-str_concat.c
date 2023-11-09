5db6824c96f936d395e1797bdb3e8c7943654c13
ksyv <viais.kevin@hotmail.fr>
2023-11-09 11:35:16 +0100

    change for 2-str

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,7 +10,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *st = NULL;
-	int len1 = 0, len2 = 0;
+	int len1 = 0, len2 = 0, lentot = 0, i = 0, j = 0;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -20,15 +20,24 @@ char *str_concat(char *s1, char *s2)
 		continue;
 	for (len2 = 0; s2[len2]; len2++)
 		continue;
-	st = malloc((len1 + len2 + 1) * sizeof(char));
+	lentot = len1 + len2;
+	st = malloc((lentot + 1) * sizeof(char));
 	if (st == NULL)
 		return(NULL);
-	for (len1 = 0; s1[len1]; len1++)
-		st[len1] = s1[len1];
-	for (len2 = 0; s2[len2]; len2++)
+	while (i < lentot)
 	{
-		st[len2 + len1] = s2[len2];
+		if (i < len1)
+		{
+			st[i] = s1[i];
+			i++;
+		}
+		else
+		{
+			st[i] = s2[j];
+			i++;
+			j++;
+		}
 	}
-	st [len2 + len1 + 1] = '\0';
+	st[lentot + 1] = '\0';
 	return (st);
 }
