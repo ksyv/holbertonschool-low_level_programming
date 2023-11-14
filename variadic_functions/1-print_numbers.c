a166eb5bcb329fb8edbc48af2fdfbe180cf7c4b0
ksyv <viais.kevin@hotmail.fr>
2023-11-14 14:36:05 +0100

    rechange for separator == NULL

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,6 +13,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (n == 0)
 		return;
+	if (separator == NULL)
+		separator = "";
 	va_start(valist, n);
 		for (index = 0; index < n; index++)
 		{
