f1661ee4858f813fa42a2043879f47f76363e15e
ksyv <viais.kevin@hotmail.fr>
2023-11-14 15:24:47 +0100

    test

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,11 +13,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (n == 0)
 		return;
+	if (separator == NULL)
+		separator = " ";
 	va_start(valist, n);
 		for (index = 0; index < n; index++)
 		{
 			printf("%d", va_arg(valist, int));
-			if (index != (n - 1) && separator != NULL)
+			if (index != (n - 1))
 				printf("%s", separator);
 		}
 	printf("\n");
