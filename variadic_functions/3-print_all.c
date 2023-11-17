e3c1f6ebce2344639623c7e9862762d98ea5e103
ksyv <viais.kevin@hotmail.fr>
2023-11-17 11:45:45 +0100

    add task 3 and change .h for this

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+/**
+ * print_char - prints a char.
+ * @arg: arguments pointed
+ */
+void print_char(va_list arg)
+{
+	char letter;
+
+	letter = va_arg(arg, int);
+	printf("%c", letter);
+}
+/**
+ * print_int - prints an int.
+ * @arg: arguments pointed
+ */
+void print_int(va_list arg)
+{
+int num;
+
+num = va_arg(arg, int);
+printf("%d", num);
+}
+/**
+ * print_float - Prints a float.
+ * @arg: arguments pointed
+ */
+void print_float(va_list arg)
+{
+float number;
+
+number = va_arg(arg, double);
+printf("%f", number);
+}
+/**
+ * print_string - prints a string.
+ * @arg: arguments pointed
+ */
+void print_string(va_list arg)
+{
+char *string;
+
+string = va_arg(arg, char *);
+
+if (string == NULL)
+{
+printf("(nil)");
+return;
+}
+
+printf("%s", string);
+}
+/**
+ * print_all - print anything
+ * @format: prints strings, list of types of arguments.
+ * @...: other arguments
+*/
+void print_all(const char * const format, ...)
+{
+	va_list listofargument;
+	unsigned int index = 0, index2 = 0;
+	char *separator = "";
+	choiceprintfunction_t typeofarg[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s",print_string},
+	};
+	va_start(listofargument, format);
+	while (format && (*(format + index)))
+	{
+		index2 = 0;
+
+		while (index2 < 4 && (*(format + index) != *(typeofarg[index2].symbol)))
+			index2++;
+
+		if (index2 < 4)
+		{
+			printf("%s", separator);
+			typeofarg[index2].print(listofargument);
+			separator = ", ";
+		}
+
+		index++;
+	}
+
+	printf("\n");
+
+	va_end(listofargument);
+}
+
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,12 @@
 #ifndef __VARIADIC__
 #define __VARIADIC__
 #include <stdarg.h>
+typedef struct choiceprintfunction
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} choiceprintfunction_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
