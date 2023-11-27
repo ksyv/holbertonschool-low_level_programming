ae4fd7e87dd7392ae5d78b20500a94bd4efc84a2
ksyv <viais.kevin@hotmail.fr>
2023-11-27 13:53:30 +0100

    add file 2-add_node.c with function who add a node at the beggining of a list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node - add a node at the beggning of the list
+ * @head: pointer to the first element of list_t
+ * @str: string of the first node
+ * Return: adress of the new element or NULL
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+	char *newStr;
+	unsigned int len = 0;
+
+	newNode =  malloc(sizeof(list_t));
+	if (newNode == NULL)
+		return (NULL);
+	newStr = strdup(str);
+	while (newStr[len] != 0)
+		len++;
+	newNode->str = newStr;
+	newNode->len = len;
+	newNode->next = *head;
+	*head = newNode;
+	return (newNode);
+}
