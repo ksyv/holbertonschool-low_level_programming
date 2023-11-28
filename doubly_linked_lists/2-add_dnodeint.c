ebaa38d45f870d6154c8ebc20f5ac621b0a22512
ksyv <viais.kevin@hotmail.fr>
2023-11-28 10:26:39 +0100

    add file 2-add_dnodeint.c with function who add a node at the beggning of the list

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_dnodeint - add a node at the beggning of the list
+ * @head: pointer to the first element of list_t
+ * @n: int of the node
+ * Return: adress of the new element or NULL
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newNode;
+	int newn;
+
+	newNode =  malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+		return (NULL);
+	newn = n;
+	newNode->n = newn;
+	newNode->next = *head;
+	newNode->prev = NULL;
+	*head = newNode;
+	return (newNode);
+}
