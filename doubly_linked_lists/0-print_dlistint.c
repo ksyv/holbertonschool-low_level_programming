7b91e5a8f2f2036ef5f75008fa7ba4ca0d2d9248
ksyv <viais.kevin@hotmail.fr>
2023-11-28 09:40:16 +0100

    add 0-print_dlistint.c with function who print all the elements of a dlist_t list.

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_dlistint - search the first node
+ * and print all the elements of a dlist_t list
+ * @h: pointer to an element of list_t
+ *Return: number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t numberOfNodes = 0;
+
+	while (h->prev != NULL)
+	{
+		h = h->prev;
+	}
+	while (h != NULL)
+	{
+
+		printf("%d\n", h->n);
+		h = h->next;
+		numberOfNodes++;
+	}
+	return (numberOfNodes);
+}
