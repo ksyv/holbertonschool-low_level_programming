146a7541f060fea294a70d3562ac3a43e3df285f
ksyv <viais.kevin@hotmail.fr>
2023-11-28 15:25:04 +0100

    add 8-delete_dnodeint.c

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * by index of a dlistint_t linked list.
+ * @h: pointer to the first element of dlist_t
+ * @idx: index of the node
+ * @n: int in the node's data
+ * Return: number of nodes
+*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temporary = *head;
+	unsigned int searchindex = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	for (searchindex = 0; searchindex != index; searchindex++)
+	{
+		if (temporary == NULL)
+			return (-1);
+		temporary = temporary->next;
+	}
+
+	if (temporary == *head)
+	{
+		*head = temporary->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+
+	else
+	{
+		temporary->prev->next = temporary->next;
+		if (temporary->next != NULL)
+			temporary->next->prev = temporary->prev;
+	}
+	free(temporary);
+	return (1);
+}
