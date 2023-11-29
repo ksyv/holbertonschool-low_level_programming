757348d7046518891664f2daaad9ac940b055b45
ksyv <viais.kevin@hotmail.fr>
2023-11-29 13:09:59 +0100

    add _putchar and 1-print_binary.c

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of a number.
+ * @num: The integer to convert into binary.
+ */
+void print_binary(unsigned long int num)
+{
+	if (num == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (num > 1)
+		print_binary(num >> 1);
+
+	_putchar((num & 1) + '0');
+}
