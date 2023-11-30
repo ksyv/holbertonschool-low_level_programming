b113252587e3cfd05431896d290f05da4a04893e
ksyv <viais.kevin@hotmail.fr>
2023-11-30 09:53:56 +0100

    add 3-set_bit.c

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit - change the value of a bit at a given index by 1.
+ * @n: number.
+ * @index: position.
+ * Return: 1 if works or -1 if error.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1;
+
+	if (index > 64)
+		return (-1);
+	if (n == 0)
+		return (0);
+	mask <<= index;
+	*n = *n | mask;
+	return (1);
+}
