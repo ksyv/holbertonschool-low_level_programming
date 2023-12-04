f99b33c6bc3ba69a5a6b2a253459c0c8bc779c07
ksyv <viais.kevin@hotmail.fr>
2023-12-04 13:50:09 +0100

    fix betty style

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * create_file - creates a file
- * @filename: file who be created
- * @text_content: text in the file
+ * append_text_to_file - appends text at the end of a file
+ * @filename: file
+ * @text_content: string to add at the end of the file
  * Return: 1 on success, -1 on failure
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fileAddCreator = 0;
 	int writer = 0;
-
+string to add at the end of the file
 	if (!filename)
 		return (-1);
 	if (!text_content)
