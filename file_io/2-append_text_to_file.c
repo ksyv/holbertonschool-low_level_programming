b9073b161bf50481e2e95c2da3f79eb4c30980d4
ksyv <viais.kevin@hotmail.fr>
2023-12-04 13:51:43 +0100

    fix an extra line

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -9,7 +9,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fileAddCreator = 0;
 	int writer = 0;
-string to add at the end of the file
+
 	if (!filename)
 		return (-1);
 	if (!text_content)
