dd3438a4906afd3c7af02df2b922808209dafbe7
ksyv <viais.kevin@hotmail.fr>
2023-12-04 13:05:11 +0100

    fix typo for betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,14 +8,14 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fileCreator = 0;
-	int writer =0;
+	int writer = 0;
 
 	if (!filename)
 		return (-1);
 	if (!text_content)
 		text_content = "";
 	/*Open*/
-	fileCreator = open(filename, O_CREAT | O_WRONLY | O_TRUNC , 0600);
+	fileCreator = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fileCreator == -1)
 		return (-1);
 
