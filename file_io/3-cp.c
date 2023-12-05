08c37eba9467a8dbeaaf7a20f2d6c827299a54de
ksyv <viais.kevin@hotmail.fr>
2023-12-05 11:04:57 +0100

    fix typo in condition line 32, reduce condition error

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -11,31 +11,25 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
-			exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fileCopied = open(argv[1], O_RDONLY);
-	if (argv[1] == NULL || fileCopied == -1)
-	{
+	if (fileCopied == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 			exit(98);
-	}
 	copyFile = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (argv[2] == NULL || copyFile == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
-			exit(99);
-	}
+	if (copyFile == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	while (reader)
 	{
 		reader = read(fileCopied, buffer, 1024);
+		if (reader == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			 exit(98);
 		if (reader > 0)
 		{
 			writer = write(copyFile, buffer, reader);
-			if (writer == -1)
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
+			if (writer == -1 || writer != reader)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 		}
 	}
 	if (close(fileCopied) == -1)
