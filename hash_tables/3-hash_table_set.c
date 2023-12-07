6fc538718047ccbf4bd8c3f00750908e0bf8e806
ksyv <viais.kevin@hotmail.fr>
2023-12-07 11:45:07 +0100

    add 3-hash_table_set.c

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - This sets the hash table with values
+ * @ht: this hives the hadh tabke to use
+ * @key: key of harsh table
+ * @value: data to input into hash table
+ * Return: 1 if successful else 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *newNode;
+	char *valueCopy;
+	unsigned long int index, index2;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	valueCopy = strdup(value);
+	if (valueCopy == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (index2 = index; ht->array[index2]; index2++)
+	{
+		if (strcmp(ht->array[index2]->key, key) == 0)
+		{
+			free(ht->array[index2]->value);
+			ht->array[i]->value = valueCopy;
+			return (1);
+		}
+	}
+
+	newNode = malloc(sizeof(hash_node_t));
+	if (newNode == NULL)
+	{
+		free(valueCopy);
+		return (0);
+	}
+	newNode->key = strdup(key);
+	if (newNode->key == NULL)
+	{
+		free(newNode);
+		return (0);
+	}
+	newNode->value = valueCopy;
+	newNode->next = ht->array[index];
+	ht->array[index] = newNode;
+
+	return (1);
+}
