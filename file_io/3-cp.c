40cdc4c6cf3b9c469dede5535db7a2eb74255d01
ksyv <viais.kevin@hotmail.fr>
2023-12-07 11:12:02 +0100

    add 1-djb2.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 			exit(98);
 	copyFile = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (copyFile == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n" argv[2]), exit(99);
 	while (reader)
 	{
 		reader = read(fileCopied, buffer, 1024);
