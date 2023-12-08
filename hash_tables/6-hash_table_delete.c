61a5583bd63c21455104ae4e45bc5e78cda31ba0
ksyv <viais.kevin@hotmail.fr>
2023-12-08 12:55:08 +0100

    add 6-hash_table_delete.c

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ *hash_table_delete - delete a hash table
+ *@ht:hash table you want to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	hash_node_t *nodeToDelete;
+	hash_node_t *temporaryNode;
+
+	if (ht == NULL || !ht->size || ht->array == NULL)
+		return;
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		for (nodeToDelete = ht->array[index]; nodeToDelete != NULL;
+			 nodeToDelete = temporaryNode)
+		{
+			temporaryNode = nodeToDelete->next;
+			free(nodeToDelete->key);
+			free(nodeToDelete->value);
+			free(nodeToDelete);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
