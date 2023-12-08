fcec4c7aaed5ef3fc2a6dc5cd9af93ae4b42048f
ksyv <viais.kevin@hotmail.fr>
2023-12-08 13:22:04 +0100

    fix 6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -4,14 +4,14 @@
  *@ht:hash table you want to delete
  */
 void hash_table_delete(hash_table_t *ht)
+
 {
 	unsigned long int index = 0;
 	hash_node_t *nodeToDelete;
 	hash_node_t *temporaryNode;
 
-	if (ht == NULL || !ht->size || ht->array == NULL)
+	if (ht == NULL || ht->size == 0 || ht->array == NULL)
 		return;
-	printf("{");
 	for (index = 0; index < ht->size; index++)
 	{
 		for (nodeToDelete = ht->array[index]; nodeToDelete != NULL;
